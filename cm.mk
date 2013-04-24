fe2e78a99717fc25082aea53c60cbe3522ac1708
UISS <uiss520@gmail.com>
2013-04-24 19:13:44 +0800

    move mb886,make qinara install and update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,16 +1,35 @@
-## Specify phone tech before including full_phone
+# Inherit device configuration for Motorola Atrix HD.
+$(call inherit-product, device/motorola/qinara/full_qinara.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-$(call inherit-product, device/motorola/msm8960-common/msm8960.mk)
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := cm_qinara
+PRODUCT_BRAND := motorola
+PRODUCT_DEVICE := qinara
+PRODUCT_MODEL := MB886
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_RELEASE_NAME := motorola Atrix HD
+PRODUCT_SFX := ATT
 
 # Boot animation
-TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
 
-# Release name
-PRODUCT_RELEASE_NAME := mb886
-PRODUCT_NAME := cm_mb886
+UTC_DATE := $(shell date +%s)
+DATE := $(shell date +%Y%m%d)
 
-$(call inherit-product, device/motorola/mb886/full_mb886.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_BRAND=motorola PRODUCT_NAME=mb886 BUILD_PRODUCT=mb886 BUILD_FINGERPRINT=motorola/mb886/qinara:4.0.4/7.7.1Q-144_VQL_S3-49/346380647:user/release-keys
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   PRODUCT_NAME=${PRODUCT_DEVICE}_${PRODUCT_SFX} \
+   BUILD_NUMBER=${DATE} \
+   TARGET_DEVICE=${PRODUCT_DEVICE} \
+   BUILD_FINGERPRINT=${PRODUCT_BRAND}/${PRODUCT_DEVICE}_${PRODUCT_SFX}/${PRODUCT_DEVICE_PREFIX}_${PRODUCT_DEVICE}:${PLATFORM_VERSION}/${BUILD_ID}/${DATE}:user/release-keys \
+   PRIVATE_BUILD_DESC="${PRODUCT_DEVICE_PREFIX}_${PRODUCT_DEVICE}-user ${PLATFORM_VERSION} ${BUILD_ID} ${DATE} release-keys" \
+   PRODUCT_BRAND=${PRODUCT_BRAND} \
+   BUILD_UTC_DATE= 
